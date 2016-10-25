06368eb22adb9aa60411f5d13355a0b63e53f9ed
adomasalcore3 <webmaster_tds96@live.com.pt>
2016-10-25 22:35:53 +0000

    Init page class

---
+++ application/libraries/System/page.php
@@ -0,0 +1,29 @@
+<?php
+/*FlameCMS CHECK*/
+defined('FlameCMS') OR exit('No script Cuddies');
+Class Page{
+	function load($page)
+	{
+		/*if triggered an redirect, cancel everything and go to page*/
+		$this->validade_page_redirect($page);
+	}
+	
+	
+	/*other....*/
+	function validade_page_redirect($page){
+		/*
+		 * ********************************
+		 * Cases of test:
+		 * ********************************
+		 * if this page is diferent of redirect
+		 * if the uri contains the lang code, remove it and test
+		 * */
+		$v=array_pop(explode('/', $page));
+		$v[0]=null;
+		$t=implode('/',$v);
+		if(defined('page_redirect') && ($page!=page_redirect) && ($t!=page_redirect))
+		{
+			
+		}
+	}
+}
