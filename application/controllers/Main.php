601b75395d46ce07737e2bc8b268cbdecfcdc170
adomasalcore3 <webmaster_tds96@live.com.pt>
2016-10-25 21:48:13 +0000

    Initiation

---
+++ application/controllers/Main.php
@@ -0,0 +1,7 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+/*Main Controller
+ * will trigger the system to start
+ * and have some other properties*/
+class Main extends CI_Controller {
+}
