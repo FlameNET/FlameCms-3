718920d08795f695f334e204de4295a4992df7c5
adomasalcore3 <webmaster_tds96@live.com.pt>
2016-10-28 09:36:17 +0000

    code update
    update footer code;
    update app css code;

--- application/views/special/footer.php
+++ application/views/special/footer.php
@@ -1,7 +1,27 @@
 <?php if(!defined('preload') || (preload==false)):?>
 		</content>
-	<footer>
-		
+	<footer  class="<?=(defined('page_redirect') && (page_redirect=='admin/install'))?'flame_cms_installer':'';?>">
+		<?php 
+				if(defined('page_redirect') && (page_redirect=='admin/install'))
+				{
+					?>
+						<div class="flamecms_preset row">
+							<div class="title small-9 medium-10 large-11 columns">
+								<p>Copyright FlameCMS &copy;<br /> All rights Reserved</p>
+							</div>
+							<div class="logo small-3 medium-2 large-1 columns">
+								<img src="<?=base_url('assets/imgs/flamecms/logo.png');?>" width="100%">
+							</div>
+						</div>
+					<?php
+				}
+				else
+				{
+					?>
+						
+					<?php
+				}
+			?>
 	</footer>
 	<?php footer();?>
 	</body>
