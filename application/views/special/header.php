e016196acb6755647c691aad1bd3631f272d6952
adomasalcore3 <webmaster_tds96@live.com.pt>
2016-10-28 20:44:06 +0000

    update favicons and header

--- application/views/special/header.php
+++ application/views/special/header.php
@@ -1,98 +1,59 @@
 <?php
 if(!defined('preload') || (preload==false)):
 ?>
+<?php 
+if(defined('page_redirect') && (page_redirect=='admin/install'))
+{
+	?>
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>FlameCMS Installer</title>
+		<!-- Enable Mobile View -->
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- Installer Favicon -->
+		<link rel="icon" href="<?=base_url('assets/imgs/flamecms/fav_normal.png');?>">
+		<link rel="shortcut icon" href="<?=base_url('assets/imgs/flamecms/fav_normal.png');?>" type="assets/image/x-icon">
+		<!--[if IE]><link rel="shortcut icon" href="<?=base_url('assets/imgs/flamecms/fav_icon.ico');?>"><![endif]-->
+        <link rel="apple-touch-icon-precomposed" href="<?=base_url('assets/imgs/flamecms/fav_normal.png');?>">
+		<meta name="msapplication-TileColor" content="#eee">
+		<meta name="msapplication-TileImage" content="<?=base_url('assets/imgs/flamecms/fav_normal.png');?>">
+		<?php head();?>
+	</head>
+	<body>
+		<header class="flame_cms_installer">
+			<div class="flamecms_preset row">
+				<div class="logo small-12 medium-4 large-2 columns">
+					<img src="<?=base_url('assets/imgs/flamecms/logo.png');?>" width="100%">
+				</div>
+				<div class="title small-12 medium-4 large-3 columns">
+					<h5>Installer</h5>
+				</div>
+			</div>
+		</header>
+		<content>
+<?php
+}
+else
+{
+?>
 <!DOCTYPE HTML>
 <html>
 	<head>
 		<title>/*the site title*/</title>
+		<!-- Enable Mobile View -->
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<?php head();?>
 	</head>
 	<body>
-		<div class="hidden_presets">
-			<div class="loader_flame">
-				<div class="container">
-				   <span class="particle lighter"></span>
-				   <span class="particle light"></span>
-				   <span class="particle light"></span>
-				   <span class="particle lighter"></span>
-				   <span class="particle dark"></span>
-				   <span class="particle dark"></span>
-				   <span class="particle light"></span>
-				   <span class="particle dark"></span>
-				   <span class="particle light"></span>
-				   <span class="particle lighter"></span>
-				   <span class="particle lighter"></span>
-				   <span class="particle light"></span>
-				   <span class="particle dark"></span>
-				   <span class="particle dark"></span>
-				   <span class="particle dark"></span>
-				   <span class="particle dark"></span>
-				   <span class="particle lighter"></span>
-				   <span class="particle light"></span>
-				   <span class="particle light"></span>
-				   <span class="particle lighter"></span>
-				   <span class="particle lighter"></span>
-				   <span class="particle lighter"></span>
-				   <span class="particle light"></span>
-				   <span class="particle dark"></span>
-				</div>
-			</div>
-		</div>
-		<header class="<?=(defined('page_redirect') && (page_redirect=='admin/install'))?'flame_cms_installer':'';?>">
-			<?php 
-				if(defined('page_redirect') && (page_redirect=='admin/install'))
-				{
-					?>
-						<div class="flamecms_preset row">
-							<div class="logo small-12 medium-4 large-2 columns">
-								<img src="<?=base_url('assets/imgs/flamecms/logo.png');?>" width="100%">
-							</div>
-							<div class="title small-12 medium-4 large-3 columns">
-								<h5>Installer</h5>
-							</div>
-						</div>
-					<?php
-				}
-				else
-				{
-					?>
-						<ul class="vertical menu large-dropdown" data-responsive-menu="drilldown large-dropdown" style="width: 300px;">
-						  <li>
-						    <a href="#">Item 1</a>
-						    <ul class="vertical menu">
-						      <li>
-						        <a href="#">Item 1A</a>
-						        <ul class="vertical menu">
-						          <li><a href="#">Item 1A</a></li>
-						          <li><a href="#">Item 1B</a></li>
-						          <li><a href="#">Item 1C</a></li>
-						          <li><a href="#">Item 1D</a></li>
-						          <li><a href="#">Item 1E</a></li>
-						        </ul>
-						      </li>
-						      <li><a href="#">Item 1B</a></li>
-						    </ul>
-						  </li>
-						  <li>
-						    <a href="#">Item 2</a>
-						    <ul class="vertical menu">
-						      <li><a href="#">Item 2A</a></li>
-						      <li><a href="#">Item 2B</a></li>
-						    </ul>
-						  </li>
-						  <li>
-						    <a href="#">Item 3</a>
-						    <ul class="vertical menu">
-						      <li><a href="#">Item 3A</a></li>
-						      <li><a href="#">Item 3B</a></li>
-						    </ul>
-						  </li>
-						</ul>
-					<?php
-				}
-			?>
+		<header class="">
+				
 		</header>
 		<content>
+<?php
+}
+?>
 <?php 
 endif;
 ?>
