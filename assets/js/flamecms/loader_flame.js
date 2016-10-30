f625cdf127bacebd0294530a9e5ef162fa89229d
adomasalcore3 <webmaster_tds96@live.com.pt>
2016-10-30 00:20:42 -0100

    Helper Jquery Functions

---
+++ assets/js/flamecms/loader_flame.js
@@ -0,0 +1,67 @@
+jQuery(document).ready(function($){
+	(function( $ ){
+		$.fn.hasAttr=function(tocheck){
+			var $this=$(this);
+			var attr=$this.attr(tocheck);
+			return (typeof attr !== typeof undefined && attr !== false);
+		};
+		$.fn.id=function(){
+			var $this=$(this);
+			if($this.hasAttr('id')){
+				return $this.attr('id');
+			}
+			else{
+				return '';
+			}
+		};
+		$.fn.name=function(){
+			var $this=$(this);
+			if($this.hasAttr('name')){
+				return $this.attr('name');
+			}
+			else{
+				return '';
+			}
+		};
+		$.keycript=function(u,successCallback){
+			errorCallback=function(){};
+			completeCallback=function(){};
+			var data={};
+			form.find('input,textarea,select').each(function(){
+				var t=$(this);
+				if((t.hasAttr('type')==true) && ((t.attr('type')=='checkbox') || (t.attr('type')=='password')))
+				{
+					if(t.attr('type')=='checkbox'){
+						var temp_checked=false;
+						if(t.is(":checked")){
+							temp_checked=true;
+						}
+						data_temp[t.id()]=temp_checked;
+					}
+					else{
+						data_temp[t.id()]=CryptoJS.MD5(t.val()).toString();
+					}
+				}
+				else{
+					if((t.attr('type')!='submit'))
+						data_temp[t.id()]=t.val();
+				}
+			});
+			$.ajax({
+			    url:u,
+			    data:data,
+			    method:'POST',
+			    success:successCallback,
+			    complete:completeCallback,
+			    error:errorCallback
+			});
+		};
+	})(jQuery);
+});
+Object.size = function(obj) {
+    var size = 0, key;
+    for (key in obj) {
+        if (obj.hasOwnProperty(key)) size++;
+    }
+    return size;
+};
