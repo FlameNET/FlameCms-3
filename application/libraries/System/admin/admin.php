baf20b5f1c5be17984f2267d35bc5c511a2165bb
adomasalcore3 <webmaster_tds96@hotmail.com>
2016-11-04 22:06:11 -0100

    creation of administration file

---
+++ application/libraries/System/admin/admin.php
@@ -0,0 +1,9 @@
+<?php
+defined('FlameCMS') or die('No Script Cuddies');
+/* **************************************************************
+ *	* For administration Functions and the "second" instance	*
+ *{ Separation Only of instances for preventing data breaches }	* 
+ * **************************************************************/
+class admin{
+	
+}
