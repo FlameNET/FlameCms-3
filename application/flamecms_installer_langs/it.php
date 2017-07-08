87d5e29d0bbcca16f0d73e7aa8f8687b61b49dca
Tiago Dias <adomasalcore3@splashframe.tk>
2017-07-08 05:23:58 +0000

    Update Installer Languages

---
+++ application/flamecms_installer_langs/it.php
@@ -0,0 +1,114 @@
+<?php
+/*script cuddies answer*/
+defined('FlameCMS') or die('No Script Cuddies');
+$sys=&get_inst();
+$sys->ilang->define_name('it','Italiano',base_url('assets/imgs/flags/it.png'));
+$sys->ilang->lang('it');
+/*Header*/
+__s('FlameCMS Installer','FlameCMS Installatore');
+__s('Installer','installatore');
+/*Footer*/
+__s('Made by','Fatto da');
+__s('with love and caffeine.','Con amore e caffeina.');
+__s('Licensed under','Licenza sotto');
+__s('GNU license','Licenza GNU');
+__s('Copyright FlameCMS','Copyright FlameCMS');
+__s('All rights Reserved','Tutti i diritti riservati');
+/*Install step 1*/
+__s('Welcome to FlameCMS','Benvenuti a FlameCMS');
+__s('Content Management System for World of Warcraft Servers.','Content Management System per i server World of Warcraft.');
+
+__s('Pick Your Language For the Installer','Scegli la lingua per l\'installatore');
+__s('Install Now','Installa ora');
+__s('No, Thanks','No grazie');
+__s('Stargazers','Stargazers');
+/*Install Step 2*/
+__s('Application Requirements','Requisiti delle applicazioni');
+__s('Modules','Moduli');
+__s('Required Version','Versione richiesta');
+__s('Installed Version','Versione installata');
+__s('Operating System','Sistema operativo');
+__s('Mysqli Client API Version','Versione API di Client Mysqli');
+__s('Apache Server Version','Versione server Apache');
+__s('PHP Version','Versione PHP');
+__s('Soap Client API','API del SOAP client');
+__s('Soap Exists and Enabled','SOAP esiste e abilitato');
+
+__s('IconV Extension Exists','Esiste l\'estensione IconV');
+__s('Curl Extension Exists','Esiste l\'estensione Curl');
+__s('Hash Extension Exists','Esiste l\'estensione Hash');
+__s('OpenSSL Extension Exists','Esiste l\'estensione OpenSSL');
+
+__s('hey! look at the person who has the best Host provider!','Hey! Guardare la persona che ha il miglior fornitore di host!');
+__s('Lets finish the install ok?','Consente di completare l\'installazione correttamente?');
+__s('Continue to Step 2','Continuare al passo 2');
+__s('Ups... it apears that you have some unsatisfied requirements...','Ups ... sembra che tu abbia qualche esigenza insoddisfatta ...');
+__s('please talk to you host provider in order to install them.','Si prega di parlare con l\'host fornitore per installarli.');
+/*Install Step 3*/
+__s('Required','necessario');
+__s('[CMS] Initiation of setup','[CMS] Avvio dell\'installazione');
+__s('[CMS] Mysql Server Connection','[CMS] Connessione del server Mysql');
+__s('[CMS] Mysql Server Configuration','[CMS] Configurazione del server Mysql');
+__s('[CMS] Mysql Admin Account','[CMS] Account amministratore Mysql');
+__s('[CMS] Administrator Account','[CMS] Account amministratore');
+__s('Not Required','Non richiesto');
+__s('[WOW] Server Configuration and Realms','[WOW] Configurazione del server e realms (solo informazioni)');
+__s('[WOW] Server Configurations and Realms','[CMS] Configurazioni del server e Realms');
+/*[CMS] Initiation of setup*/
+__s('Default CMS Language','Lingua CMS predefinita');
+__s('Domain of this CMS','Dominio di questo CMS');
+__s('CMS Public Name','Nome pubblico CMS');
+__s('Multi Language?','Multi lingua?');
+__s('Yes','sì');
+__s('No','No');
+__s('Languages','Le lingue');
+__s('Changing the default language will reset the language list (the checkboxes)','Cambiare la lingua predefinita reimposterà l\'elenco delle lingue (le caselle di controllo)');
+__s('This is unnecessary, because you can do it on the admin panel','Questo non è necessario, perché è possibile farlo nel pannello di amministrazione');
+__s('Send Data','Invia dati');
+/*[CMS] Mysql Settings*/
+__s('[CMS] Mysql Host','[CMS] Host Mysql');
+__s('80.00.00.00 or flamecms.github.io','');
+__s('[CMS] Mysql User','[CMS] Utente Mysql');
+__s('dbuser','utentedb');
+__s('[CMS] Mysql Password','[CMS] Password Mysql');
+__s('excelentPassword','Eccellente_password');
+__s('[CMS] Mysql Port','[CMS] Porta Mysql');
+__s('Port','Porta');
+__s('[CMS] Database Name','[CMS] Nome del database');
+__s('greatdb','grandedb');
+__s('[CMS] Database Table Prefix','[CMS] Prefisso della tabella di database');
+__s('tableprexif_','Tabellaprefisso_');
+__s('Check [CMS] Mysql Settings','[CMS] Controllare le impostazioni di Mysql');
+/*[CMS] Mysql Settings Connection Errors*/
+__s('Everything Ok! procced','Tutto ok! Dirigersi');
+__s('Ups, Apears that your Connection Is not Right. Please Check Your Settings','Ups, appare che la connessione non è giusta. Controlla le impostazioni');
+__s('The Database Does not exist, please check it','Il database non esiste, per favore controlla');
+/*[CMS] Admin Account*/
+__s('Admin Username','Username dell\'amministratore');
+__s('Admin_username123','amministratore_username123');
+__s('Admin Password','Password dell\'amministratore');
+__s('super_admin_p455w0rd','super_admin_p455w0rd');
+__s('Confirm the Admin Password','Confermare la password amministratore');
+__s('Admin Email','Email amministratore');
+__s('admin@flamecms.github.io','admin@flamecms.github.io');
+__s('Admin First Name','Nome dell\'amministratore');
+__s('John','Giovanni');
+__s('Admin Last Name','Cognome dell\'amministratore');
+__s('Doe','Daino');
+/*[CMS] wow server and realms*/
+__s('Add WOW Server','Aggiungi WOW Server');
+__s('','');
+__s('To continue, please fill all the information and submit the form.','Per continuare, compilare tutte le informazioni e inviare il modulo.');
+__s('With all done, you will be redirected to the administation page and you will be able to and the wow server.','Con tutto ciò, sarai reindirizzato alla pagina di amministrazione e sarai in grado di e il server wow.');
+/*Install End Fenching Current Status...*/
+__s('Feching Current Status...','Recupero dello stato corrente...');
+__s('','');
+__s('','');
+__s('','');
+__s('','');
+__s('','');
+__s('','');
+
+/*
+__s('','');
+ * */
